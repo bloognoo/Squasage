31521344c85f8b75daefed4a7c55fe29ffbf74d9
Harry French <harryf@idonix.com>
2015-05-30 23:25:41 +0100

    Primordial touch classes

---
+++ Source/Touchpad.hx
@@ -0,0 +1,17 @@
+/*package;
+
+import openfl.events.EventDispatcher;
+
+
+class TouchPad extends EventDispatcher
+{
+	public function new()
+	{
+		super();
+	}
+
+	public function broadcast( id:String ):Void
+	{
+		dispatchEvent(new TouchEvent(id));
+	}
+}*/
